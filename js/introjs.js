a0ab92be74b8659c1e964771f7294a5294f127a7
afernandezm-esliceu <afernandezm@esliceu.net>
2025-01-13 19:55:20 +0100

    Javascript

---
+++ js/introjs.js
@@ -0,0 +1,39 @@
+var texto = "hola mundo";
+console.log(texto);
+
+texto = "Adios Mundo";
+console.log(texto)
+
+var a = 1
+var b = 10
+
+console.log(a + b)
+
+if (a>b) {
+    console.log("a es mayor que b");
+
+} else if(b<a){
+    console.log("b es mayor que a");
+} else {
+    console.log("IGUALDAD");
+}
+
+// b vale 5
+
+while (b>0){
+    console.log("b vale: " + b-1);
+    b--;
+}
+
+b=5
+for (var i=b; i>0; i--){
+    console.log("b vale: " +i);
+}
+
+var c;
+console.log(c)
+c= 1;
+
+if (c>0){
+    console.log("C es mayor que 0")
+}
