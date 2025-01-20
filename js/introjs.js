b81e6f9482d70232cbafe77af5fc58a3033d1d8c
afernandezm-esliceu <afernandezm@esliceu.net>
2025-01-20 20:40:30 +0100

    Dom en javascript

--- js/introjs.js
+++ js/introjs.js
@@ -37,3 +37,48 @@ c= 1;
 if (c>0){
     console.log("C es mayor que 0")
 }
+
+var lista = ["Pedro", "Dani"];
+
+console.log(lista[0])
+console.log(lista[1])
+
+lista[1] = "Vico";
+
+console.log(lista[1])
+
+lista.push("Agus");
+
+console.log(lista)
+
+for (i=0; i<lista.length; i++){
+    console.log(lista[i] + " tiene un 10.")
+}
+
+var lista2 = [1, 2, 3, 4, "Daniel"]
+
+for (i=0; i<lista2.length; i++){
+    if (lista[i]==undefined) continue;
+    if(!isNaN(lista2[i])){
+        console.log("La posición " + i + " es un numero y vale" + lista2[i] + ".")
+    } else{
+        console.log("La posición " + i + "  NO es un numero y vale" + lista2[i] + ".")
+    }
+}
+
+function excelente(nombre){
+    console.log(nombre + " tiene un 10.");
+}
+
+var edad = prompt("Cual es tu edad?")
+
+if(isNaN(edad)){
+    edad = prompt("No has introducido un numero. Cual es tu edad?")
+} else {
+
+if(edad >=18){
+    alert("Eres mayor de edad");
+} else{
+    alert("No puedes acceder a este sitio web");
+}
+}
