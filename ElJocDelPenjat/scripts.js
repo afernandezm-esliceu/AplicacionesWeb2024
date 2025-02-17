7f190085ba6d9314f1b905dabe86ec7c4e4af7f6
afernandezm-esliceu <afernandezm@esliceu.net>
2025-02-17 20:22:41 +0100

    cambios

--- ElJocDelPenjat/scripts.js
+++ ElJocDelPenjat/scripts.js
@@ -1,4 +1,30 @@
-const time = document.querySelector("#time")
+const time = document.querySelector("time");
+const wordContainer = document.getElementById("wordContainer");
+const startButton = document.getElementById('startButton');
+const usedLettersElement = document.getElementById("usedLetters");
+
+let selectWord;
+let usedLetters;
+let mistakes;
+let hits;
+
+const updateHangmanImage = () => {
+    let imageNumber = Math.min(mistakes + 1, 7);
+    hangmanImage.src = `img0${imageNumber}.svg`;
+};
+
+const startGame = () => {
+    usedLetters = [];
+    mistakes = 0;
+    hits = 0;
+    wordContainer.innerHTML = '';
+    usedLettersElement.innerHTML = '';
+    startButton.style.display = 'none';
+    drawHangMan();
+}
+
+startButton.addEventListener('click', startGame);
+
 
 let segundos = 0
 
